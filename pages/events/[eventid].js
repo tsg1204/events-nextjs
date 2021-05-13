bf41fafdff91dce8b72ea9550bd4ba3c79311a85
Tatiana Grigorieva <tsg1204@gmail.com>
2021-05-12 22:17:45 -0400

    event detail page ssetup

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,14 +1,33 @@
 import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import { Fragment } from 'react';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
-export default function EventDetailPage() {
+export default function AllEventsPage() {
   const router = useRouter();
 
-  console.log(router.pathname);
-  console.log(router.query);
+  //console.log(router.pathname);
+  const eventId = router.query.eventid;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
 
   return (
-    <div>
-      <h1>The Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
